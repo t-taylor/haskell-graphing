cb980d9f4d092c5bd24ff6ef3b9e566ee0af90e3
Tom Taylor <txt452@student.bham.ac.uk>
2017-09-06 11:05:18 +0100

    Started on better screen scaling

--- src/Bindings.hs
+++ src/Bindings.hs
@@ -3,17 +3,55 @@ module Bindings
   ) where
 import Graphics.UI.GLUT
 
+-- Better DisplayCallback
+displayRange :: Double -> Double -> (Double -> Double) -> PrimitiveMode -> DisplayCallback
+displayRange min max f brush = do
+  clear [ColorBuffer]
+  renderPrimitive brush $
+    mapM_ (\(x, y) -> vertex $ Vertex3 x y 0) $ scaledCoords min max f
+  flush
+
+scaledCoords :: Double -> Double -> (Double -> Double) -> [(Double, Double)]
+scaledCoords min max f = xyscale $ genPoint min max ((max - min) / 1000) f
+
+genPoint n max res f = if n >= max
+                          then [(n, f n)]
+                          else (n, f n) : genPoint (n + res) max res f
+
+-- I should really use matricies instead (This is also incorrect at the
+-- moment)
+xyscale :: (Fractional t, Fractional t1, Ord t, Ord t1) => [(t1, t)] -> [(t1, t)]
+xyscale xs = reduce (maxvals xs) (minvals xs) xs
+  where reduce (maxx, maxy) (minx, miny) ((x, y) : ys) 
+          = (moddedx , moddedy) : reduce (mx, my) ys
+          where moddedx = (x - minx) * (2 / (maxx - minx))
+                moddedy = (y - miny) * (2 / (maxy - miny))
+        reduce _ [] = []
+
+maxvals :: (Ord t, Ord t1, Num t1, Num t) => [(t1, t)] -> (t1, t)
+maxvals [(x,y)] = (x, y)
+maxvals (xt:ys) = maxt xt $ maxvals ys
+  where maxt (x,y) (j,k) = (max x j, max y k)
+maxvals [] = (0, 0)
+
+minvals :: (Ord t, Ord t1, Num t1, Num t) => [(t1, t)] -> (t1, t)
+minvals [(x,y)] = (x, y)
+minvals (xt:ys) = mint xt $ minvals ys
+  where mint (x,y) (j,k) = (min x j, min y k)
+minvals [] = (0, 0)
+
+
 -- Create a DisplayCallback from a function and resolution
 displayLine :: (Double -> Double) -> Double -> DisplayCallback
 displayLine f res = do
   clear [ColorBuffer]
   renderPrimitive Points $
-    mapM_ (\(x, y) -> vertex $ Vertex3 x y 0) $ gscale $ graph res f 0
+    mapM_ (\(x, y) -> vertex $ Vertex3 x y 0) $ yscale $ graph res f 0
   flush
 
 -- Scale graph
-gscale :: [(Double, Double)] -> [(Double, Double)]
-gscale ys = reduce (maxval ys) ys
+yscale :: [(Double, Double)] -> [(Double, Double)]
+yscale ys = reduce (maxval ys) ys
   where reduce val ((x, y):ys) = (x, 2 * (y / val) - 1) : reduce val ys
         reduce _ [] = []
         maxval [(x,y)] = y
